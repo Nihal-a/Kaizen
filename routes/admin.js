fb9058a801a8ebda29606b22a2b5aeb3619afeb9
Nihal-a <nihalavulan1@gamil.com>
2021-12-22 21:49:37 +0530

    Admin routes creted

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res) {
   res.render('admin/adminHome',{admin:true});
 });
 
+router.get('/applications', function(req, res) {
+  res.render('admin/applications',{admin:true});
+});
+
+router.get('/blogs', function(req, res) {
+  res.render('admin/blogs',{admin:true});
+});
+
+router.get('/queries', function(req, res) {
+  res.render('admin/queries',{admin:true});
+});
+
+router.get('/testimonials', function(req, res) {
+  res.render('admin/testimonials',{admin:true});
+});
+
+router.get('/applications', function(req, res) {
+  res.render('admin/applications',{admin:true});
+});
+
+
+router.get('/logout', function(req, res) {
+  res.render('admin/applications',{admin:true});
+});
+
+
+
 module.exports = router;
